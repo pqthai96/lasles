6bbf7ea380bc7e4e413ab78b303cb7364ab87583
pqthai96 <111183239+pqthai96@users.noreply.github.com>
2023-11-24 15:47:09 +0700

    commit

---
+++ assets/scripts/app.js
@@ -0,0 +1,8 @@
+$('.customer-list').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  dots: true,
+  prevArrow:"<button type='button' class='slick-prev pull-left'><img src='./assets/images/icon/arrow-left.svg'></button>",
+  nextArrow:"<button type='button' class='slick-next pull-right'><img src='./assets/images/icon/arrow-right.svg'></button>"
+});
